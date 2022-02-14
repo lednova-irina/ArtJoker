d5d9c578912fa00b76f9a1271235977be9eb600e
Irene <irene.lednev@gmail.com>
2022-02-14 18:15:24 +0200

    code refactoring

--- practice/trainee_js_task3.js
+++ practice/trainee_js_task3.js
@@ -4,55 +4,93 @@
 // новый элемент, удалить элемент, найти элемент по его значению)
 
 class Node {
-  constructor(incomeData) {
+  constructor(incomeData, left = 0, right = 0) {
     this.data = incomeData;
-    this.left = null;
-    this.right = null;
+    this.left = left;
+    this.right = right;
   }
 }
 
 class BinarySearchTree {
   constructor() {
     this.root = null;
+    this.count = 0;
   }
 
   insert(data) {
+    this.count++;
     const newNode = new Node(data);
     if (!this.root) {
       this.root = newNode;
-      return;
+    } else {
+      this.insertNode(this.root, newNode);
     }
-    this.insertNode(this.root, newNode);
   }
 
-  insertNode(currentNode, newNode) {
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (!node.left) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (!node.right) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  remove(data) {}
+
+  search(data) {
+    let currentNode = this.root;
     while (currentNode) {
-      if (newNode.value < currentNode.value) {
-        if (!currentNode.left) {
-          currentNode.left = newNode;
+      if (currentNode.data !== data) {
+        if (currentNode.data < data) {
+          currentNode = currentNode.right;
+        } else {
+          currentNode = currentNode.left;
         }
-        this.insertNode(currentNode.left, newNode);
       }
-      if (!currentNode.right) {
-        currentNode.right = newNode;
-      }
-      this.insertNode(currentNode.right, newNode);
+      return currentNode;
     }
   }
-  remove() {}
-  search() {}
-}
 
-const binaryTree = new BinarySearchTree();
-binaryTree.insert(8)
-binaryTree.insert(5)
-binaryTree.insert(10)
-binaryTree.insert(81)
-binaryTree.insert(6)
-binaryTree.insert(58)
-binaryTree.insert(88)
-binaryTree.insert(2)
-console.log(binaryTree)
+  size() {
+    return this.count;
+  }
+  getMin() {
+    let currentNode = this.root;
+    while (currentNode.left) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.data;
+  }
+  getMax() {
+    let currentNode = this.root;
+    while (currentNode.right) {
+      currentNode = currentNode.right;
+    }
+    return currentNode.data;
+  }
+  contains(data) {
+    let currentNode = this.root;
+    while (currentNode) {
+      if (currentNode.data === data) {
+        return true;
+      }
+      if (currentNode.data < data) {
+        currentNode = currentNode.right;
+      } else {
+        currentNode = currentNode.left;
+      }
+    }
+    return false;
+  }
+}
 
 // 2.Написать сортировку двумя различными методами (Можно выбрать любые методы сортировки, самые простые:
 // пузырьковая, выбором)
